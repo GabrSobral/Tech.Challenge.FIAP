855061a30012519b6cfe8ac581327116a7ec0a5a
Gabriel Sobral <gabriel.sobral1367@gmail.com>
2026-01-21 20:32:36 -0300

    Add logging to devug

--- src/Tech.Challenge/Program.cs
+++ src/Tech.Challenge/Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Tech.Challenge.Application;
 using Tech.Challenge.Configuration;
 using Tech.Challenge.Infra.Database;
@@ -5,8 +6,6 @@ using Tech.Challenge.Infra.MailService;
 using Tech.Challenge.Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services
     .ConfigureOptions(builder.Configuration)
@@ -20,6 +19,23 @@ builder.Services
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    var remoteIp = context.Connection.RemoteIpAddress;
+
+    // Log only if NOT local
+    if (remoteIp != null && !IPAddress.IsLoopback(remoteIp))
+    {
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogInformation("External Request: {Method} {Path} from {IP}", 
+            context.Request.Method, 
+            context.Request.Path, 
+            remoteIp);
+    }
+
+    await next();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
